417e430c82c202f7188c91436e2394e78041c5ae
matheus <matheusalexandre.sena99@gmail.com>
2023-08-26 20:54:35 -0300

    feat: cadastro de 'tweets'

--- frontend/src/components/Panels/Right.tsx
+++ frontend/src/components/Panels/Right.tsx
@@ -1,26 +1,57 @@
 'use client';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { UserContext } from '@/context/user.context';
+import TweetForm from '@/components/TweetForm';
 
 export default function Right() {
+  const [showModal, setShowModal] = useState(false);
+
   const { user } = useContext(UserContext);
 
   return (
-    <div className="flex h-screen p-4 flex-col">
-      <p className="font-bold text-3xl mb-8">λ-Social</p>
+    <>
+      {showModal ? (
+        <>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50">
+            <div className="relative w-2/5 my-6 mx-auto">
+              <div className="border-0 rounded-md shadow-lg relative flex flex-col w-full bg-white">
+                <div className="flex items-start justify-between px-5 pb-5">
+                  <button
+                    className="ml-auto bg-transparent border-0 text-black float-right text-3xl leading-none font-semibold"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="bg-transparent text-black h-6 w-6 text-2xl block">
+                      ×
+                    </span>
+                  </button>
+                </div>
+                <TweetForm />
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
 
-      <div className="border-2 p-2 rounded-lg hover:cursor-pointer hover:bg-gray-100 transition-all duration-300">
-        <p className="font-bold">
-          {user?.firstName} {user?.lastName}
-        </p>
-        <p className="font-light text-sm">@{user?.username}</p>
+      <div className="flex h-screen p-4 flex-col">
+        <p className="font-bold text-3xl mb-8">λ-Social</p>
+
+        <div className="border-2 p-2 rounded-lg hover:cursor-pointer hover:bg-gray-100 transition-all duration-300">
+          <p className="font-bold">
+            {user?.firstName} {user?.lastName}
+          </p>
+          <p className="font-light text-sm">@{user?.username}</p>
+        </div>
+
+        <div className="flex-1" />
+
+        <button
+          className="w-full ml-auto mb-3 mr-3 text-white bg-primary-600 hover:bg-primary-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+          onClick={() => setShowModal(true)}
+        >
+          Postar nova λ
+        </button>
       </div>
-
-      <div className="flex-1" />
-
-      <button className="w-full ml-auto mb-3 mr-3 text-white bg-primary-600 hover:bg-primary-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
-        Postar nova λ
-      </button>
-    </div>
+    </>
   );
 }
--- frontend/src/components/TweetForm.tsx
+++ frontend/src/components/TweetForm.tsx
@@ -1,9 +1,68 @@
+'use client';
+
+import { useContext, useTransition } from 'react';
+import { UserContext } from '@/context/user.context';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { useRouter } from 'next/navigation';
+
+type InputTweet = {
+  insertContent: string;
+  insertPostUserId: string;
+};
+
 export default function TweetForm() {
+  const router = useRouter();
+  const { user } = useContext(UserContext);
+
+  const [isPending, startTransition] = useTransition();
+
+  const { register, handleSubmit, reset } = useForm<InputTweet>();
+  const onSubmit: SubmitHandler<InputTweet> = (data) => {
+    if (!user) {
+      alert('Você precisa estar logado para postar uma λ');
+      return;
+    }
+
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+
+    var requestOptions: RequestInit = {
+      method: 'PUT',
+      headers: headers,
+      body: JSON.stringify({
+        ...data,
+        insertPostUserId: user?.userUserId,
+        insertPostType: 'Default',
+      }),
+      redirect: 'follow',
+    };
+
+    fetch('http://localhost:8080/post', requestOptions)
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .then(() => {
+        reset();
+      })
+      .then(() => {
+        startTransition(() => {
+          router.refresh();
+        });
+      })
+      .catch((error) => {
+        console.log('error', error);
+        alert('Erro ao postar uma λ. Tente novamente.');
+      });
+  };
+
   return (
-    <div className="w-full h-[25vh] border-b-2 flex flex-col">
+    <form
+      className="w-full h-[25vh] border-b-2 flex flex-col"
+      onSubmit={handleSubmit(onSubmit)}
+    >
       <textarea
         className="outline-none w-full resize-none p-4 h-full"
         placeholder="O que você está pensando?"
+        {...register('insertContent', { required: true })}
       ></textarea>
 
       <div className="flex">
@@ -14,6 +73,6 @@ export default function TweetForm() {
           Postar
         </button>
       </div>
-    </div>
+    </form>
   );
 }
