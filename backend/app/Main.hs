012e77c51fcf713978b5bc4fe3c708253b9c0b1c
matheus <matheusalexandre.sena99@gmail.com>
2023-08-28 16:01:10 -0300

    feat: websocket chat implementado

--- backend/app/Main.hs
+++ backend/app/Main.hs
@@ -8,20 +8,16 @@ import Data.Aeson (ToJSON, FromJSON)
 import Network.Wai.Middleware.Cors
 import Network.Wai.Handler.Warp (run)
 import API (api)
-import qualified Websocket as Websocket
 import qualified Database.PostgreSQL.Simple as PGSimple
 import qualified Controller.UserController as UserController
 import qualified Controller.PostController as PostController
 import qualified Controller.FollowController as FollowController
 import qualified Controller.IdenticonController as IdenticonController
-import qualified Migrations
-import System.IO (hFlush, stdout)
-import qualified Network.WebSockets as WS
-import Control.Concurrent (MVar, newMVar, forkIO)
-
+import Control.Concurrent (forkIO)
 import Control.Monad (void)
-import Network.Wai.Handler.WebSockets (websocketsOr)
-import Network.WebSockets (defaultConnectionOptions)
+import qualified Migrations
+import qualified Websocket as Ws
+import System.IO (hFlush, stdout)
 
 main :: IO ()
 main = do
@@ -37,42 +33,38 @@ main = do
   -- Create tables if they don't exist
   Migrations.createTables conn
 
+  -- run websocket server
+  putStrLn "Running websocket server on port 9160.."
+  void $ forkIO Ws.runServer
 
   let frontCors = simpleCorsResourcePolicy { corsOrigins = Just (["http://localhost:3000"],  True)
                                            , corsMethods = ["DELETE", "GET", "PUT", "POST", "PATCH"]
                                            , corsRequestHeaders = ["Authorization", "Content-Type"] }
 
-
-  putStrLn "Running websocket server on port 9160.."
-  state <- newMVar Websocket.newServerState
-  void $ forkIO $ WS.runServer "localhost" 9160 $ Websocket.application state
-
-    -- Run the Servant server
-  putStrLn "Running api server on port 8080.."
-  run 8080 $ cors (const $ Just $ frontCors) $ app state conn
+  -- Run the Servant server
+  putStrLn "Running server on port 8080.."
+  hFlush stdout  -- Flush the buffer to ensure immediate display
+  run 8080 $ cors (const $ Just $ frontCors) $ (serve API.api (
+                                UserController.getAllUsersHandler conn
+                           :<|> UserController.getUserByUsernameHandler conn
+                           :<|> UserController.getUserByEmailHandler conn
+                           :<|> UserController.getUserByIdHandler conn
+                           :<|> UserController.insertUserHandler conn
+                           :<|> UserController.updateUserHandler conn
+                           :<|> UserController.deleteUserHandler conn
+                           :<|> PostController.getAllPostsHandler conn
+                           :<|> PostController.getPostsByUserIdHandler conn
+                           :<|> PostController.getPostsByFollowsHandler conn
+                           :<|> PostController.getPostByIdHandler conn
+                           :<|> PostController.insertPostHandler conn
+                           :<|> PostController.updatePostHandler conn
+                           :<|> PostController.deletePostHandler conn
+                           :<|> FollowController.getFollowingHandler conn
+                           :<|> FollowController.getFollowersHandler conn
+                           :<|> FollowController.insertFollowHandler conn
+                           :<|> (FollowController.deleteFollowHandler conn)
+                           :<|> (IdenticonController.generateIdenticonHandler)
+                          ))
 
   -- Close the connection
   PGSimple.close conn
-
-app :: MVar Websocket.ServerState -> PGSimple.Connection -> Application
-app state conn = websocketsOr defaultConnectionOptions (Websocket.application state) $ serve API.api (
-  (UserController.getAllUsersHandler conn)
-  :<|> (UserController.getUserByUsernameHandler conn)
-  :<|> (UserController.getUserByEmailHandler conn)
-  :<|> (UserController.getUserByIdHandler conn)
-  :<|> (UserController.insertUserHandler conn)
-  :<|> (UserController.updateUserHandler conn)
-  :<|> (UserController.deleteUserHandler conn)
-  :<|> (PostController.getAllPostsHandler conn)
-  :<|> (PostController.getPostsByUserIdHandler conn)
-  :<|> (PostController.getPostsByFollowsHandler conn)
-  :<|> (PostController.getPostByIdHandler conn)
-  :<|> (PostController.insertPostHandler conn)
-  :<|> (PostController.updatePostHandler conn)
-  :<|> (PostController.deletePostHandler conn)
-  :<|> (FollowController.getFollowingHandler conn)
-  :<|> (FollowController.getFollowersHandler conn)
-  :<|> (FollowController.insertFollowHandler conn)
-  :<|> (FollowController.deleteFollowHandler conn)
-  :<|> (IdenticonController.generateIdenticonHandler)
-  )
