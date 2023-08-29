da0d2400918c5c0617ec54d91987730cc1e2dc55
Pedro Shoji <pedroshoji@hotmail.com>
2023-08-28 23:40:19 -0300

    passwordEncryption

--- backend/test/Utils/PasswordEncryptionSpec.hs
+++ backend/test/Utils/PasswordEncryptionSpec.hs
@@ -14,6 +14,8 @@ spec = do
     prop "should be equal for the same password" $ do
       \password -> encryptPassword password `shouldBe` encryptPassword password
 
-  -- context "When checking a password" $ do
-  --   prop "should be equal to its encrypted version" $ do
-  --     \password -> checkPassword password (encryptPassword password)
+    it "returns predictable value" $ do
+      encryptPassword "as12ma*3-@r" `shouldBe` "4872df144e79a8558f39f771bb1c98a704c0a5719c617249899296ba7c16e0ad"
+
+    it "returns expected value" $ do
+      encryptPassword "12345" `shouldBe` "5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5"
